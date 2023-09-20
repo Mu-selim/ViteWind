46e9d1b12c72f5516ed3af222368ef123e544e0b
Muhammad Selim <muhamadselim70@gmail.com>
2023-09-20 19:37:26 +0300

    0.4.0

--- templates/react/js/src/App.jsx
+++ templates/react/js/src/App.jsx
@@ -27,21 +27,21 @@ function App() {
       </div>
       <h1 className="mt-4 text-5xl font-semibold">ViteWind + React</h1>
       <div className="flex flex-col items-center justify-center gap-4 text-center mt-8">
-        <button onClick={() => setCount((count) => count + 1)}
-        className="px-4 py-1 text-lg font-semibold text-white transition-all duration-200 bg-[#183D3D] rounded-md hover:bg-[#7B9E8F]"
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="px-4 py-1 text-lg font-semibold text-white transition-all duration-200 bg-[#183D3D] rounded-md hover:bg-[#7B9E8F]"
         >
           count is {count}
         </button>
-        <p
-        className="text-sm text-[#93B1A6]" 
-        >
-          Edit <code
-          className="px-1 py-1 text-sm font-semibold text-white bg-[#183D3D] rounded-sm"
-          >src/App.jsx</code> and save to test HMR
+        <p className="text-sm text-[#93B1A6]">
+          Edit{" "}
+          <code className="px-1 py-1 text-sm font-semibold text-white bg-[#183D3D] rounded-sm">
+            src/App.jsx
+          </code>{" "}
+          and save to test HMR
         </p>
       </div>
-      <p className="mt-8 text-[#93B1A6]"
-      >
+      <p className="mt-8 text-[#93B1A6]">
         Click on the ViteWind and React logos to learn more
       </p>
     </div>
